33eb384791563e47ce3c7e1a8f7435f755076322
Emiliano Vavassori <syntaxerrormmm@gmail.com>
2015-07-28 22:40:07 +0200

    Added configuration for domain operators.

--- roles/domain/templates/sudoers.d
+++ roles/domain/templates/sudoers.d
@@ -2,3 +2,5 @@
 # Autorizzazione per gli utenti di dominio
 %domain\ admins ALL=(ALL:ALL) NOPASSWD:ALL
 %{{ domainshort }}\\domain\ admins ALL=(ALL:ALL) NOPASSWD:ALL
+%domain\ operators ALL=(ALL:ALL) ALL
+%{{ domainshort }}\\domain\ operators ALL=(ALL:ALL) ALL
