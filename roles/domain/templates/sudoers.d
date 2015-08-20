ea64c6f599a660e0d0e874d05330b4ca0d88008e
Emiliano Vavassori <syntaxerrormmm@gmail.com>
2015-08-21 01:41:17 +0200

    Fixing operators superpowers on sudoers.d.

--- roles/domain/templates/sudoers.d
+++ roles/domain/templates/sudoers.d
@@ -2,5 +2,5 @@
 # Autorizzazione per gli utenti di dominio
 %domain\ admins ALL=(ALL:ALL) NOPASSWD:ALL
 %{{ domainshort }}\\domain\ admins ALL=(ALL:ALL) NOPASSWD:ALL
-%domain\ operators ALL=(ALL:ALL) ALL
-%{{ domainshort }}\\domain\ operators ALL=(ALL:ALL) ALL
+%domopers ALL=(ALL:ALL) ALL
+%{{ domainshort }}\\domopers ALL=(ALL:ALL) ALL
