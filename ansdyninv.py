ce71b22055158cc0fbc038e6db211ad03999a869
Emiliano Vavassori <syntaxerrormmm@gmail.com>
2015-09-07 21:12:03 +0200

    Updated ansdyninv.py.

--- ansdyninv.py
+++ ansdyninv.py
@@ -2,19 +2,31 @@
 # vim:sts=2:sw=2
 
 from argparse import ArgumentParser
+from os import popen
 import requests
 
-parser = ArgumentParser()
+parser=ArgumentParser()
 parser.add_argument('--list', help="Returns a JSON dictionary of all the groups to be managed.", action="store_true")
 parser.add_argument('--host', nargs='?', help="Returns an empty JSON dictionary.")
-args = parser.parse_args()
+args=parser.parse_args()
+
+# Simplifying, since we haven't already installed domain.yml, we assume to use
+# default gateway IP.
+gwt=popen("ip route show | grep ^default")
+data=gwt.read()
+gwt.close()
+srv=data.split()[2]
 
 if args.list:
-  r = requests.get("http://localhost:3000/ansible_inventory")
+  r = requests.get("http://" + srv + ":3000/ansible_list")
   if r.status_code != 200:
     sys.exit(1)
   
   print r.text
 
 if args.host:
-  print "{}"
+  r = requests.get("http://" + srv + ":3000/ansible_host")
+  if r.status_code != 200:
+    sys.exit(1)
+  
+  print r.text
