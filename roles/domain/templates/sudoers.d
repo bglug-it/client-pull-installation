e9150df4adee45df8fec49a32240e26f93dad46e
Emiliano Vavassori <syntaxerrormmm@gmail.com>
2015-09-21 01:29:34 +0200

    Local domain variables are loaded as part of local facts, as done with the server. Changed all tasks that require domain informations.

--- roles/domain/templates/sudoers.d
+++ roles/domain/templates/sudoers.d
@@ -1,6 +1,6 @@
 # {{ ansible_managed }}
 # Autorizzazione per gli utenti di dominio
 %domain\ admins ALL=(ALL:ALL) NOPASSWD:ALL
-%{{ domainshort }}\\domain\ admins ALL=(ALL:ALL) NOPASSWD:ALL
+%{{ ansible_local.domain.domainshort }}\\domain\ admins ALL=(ALL:ALL) NOPASSWD:ALL
 %domopers ALL=(ALL:ALL) ALL
-%{{ domainshort }}\\domopers ALL=(ALL:ALL) ALL
+%{{ ansible_local.domain.domainshort }}\\domopers ALL=(ALL:ALL) ALL
